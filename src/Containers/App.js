d8f73b7ade9096328f0ecff8d7956cef9768be29
Bogdan <rotaru2594@gmail.com>
2020-05-04 19:50:57 +0300

    Deploying robofriends

---
+++ src/Containers/App.js
@@ -0,0 +1,56 @@
+import React, { Fragment, Component } from 'react';
+import CardList from '../Components/CardList';
+import SearchBox from '../Components/SearchBox';
+import ErrorBoundry from '../Components/ErrorBoundry';
+import './App.css';
+import Scroll from '../Components/Scroll';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: '' 
+        }
+    }
+    componentDidMount () {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then(users => this.setState({ robots: users}))
+            
+        
+    }
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        });
+        if(robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <Fragment>
+                    <div className="tc">
+                        <h1 className='f2'>RoboFriends</h1>
+                        <SearchBox searchChange={this.onSearchChange}/>
+                        <Scroll>
+                            <ErrorBoundry>
+                                <CardList robots={filteredRobots}/>
+                            </ErrorBoundry>
+                            
+                        </Scroll>
+                    </div>
+                </Fragment>
+            )
+        }
+      
+    }
+}
+
+export default App;
