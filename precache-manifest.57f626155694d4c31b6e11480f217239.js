abe45000bc4b590160bb14dc35a999cc9f158cbe
Bogdan <rotaru2594@gmail.com>
2020-05-04 19:50:24 +0300

    Updates

---
+++ precache-manifest.57f626155694d4c31b6e11480f217239.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8f5b2218bb5854c027194a965db11d",
+    "url": "/bogdanR25/robofriendsv2/index.html"
+  },
+  {
+    "revision": "81cae17b3a53ca6d4ec5",
+    "url": "/bogdanR25/robofriendsv2/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "cd0a1c7f86213d3e09f3",
+    "url": "/bogdanR25/robofriendsv2/static/css/main.50b273f4.chunk.css"
+  },
+  {
+    "revision": "81cae17b3a53ca6d4ec5",
+    "url": "/bogdanR25/robofriendsv2/static/js/2.dc07e648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bogdanR25/robofriendsv2/static/js/2.dc07e648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0a1c7f86213d3e09f3",
+    "url": "/bogdanR25/robofriendsv2/static/js/main.5ea74ebf.chunk.js"
+  },
+  {
+    "revision": "9fbf439ebf2519ae1788",
+    "url": "/bogdanR25/robofriendsv2/static/js/runtime-main.484ecf31.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/bogdanR25/robofriendsv2/static/media/SEGA.12a3db8b.woff"
+  }
+]);
